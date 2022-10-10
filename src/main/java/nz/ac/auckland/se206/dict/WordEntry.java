1cb7fffe0a65e6b46fa026eb5d989200a15fa088
Aarnob Guha <aarnobguha@gmail.com>
2022-10-10 19:19:29 +1300

    feat: added dictionary classes and dependency to quick draw project

---
+++ src/main/java/nz/ac/auckland/se206/dict/WordEntry.java
@@ -0,0 +1,24 @@
+package nz.ac.auckland.se206.dict;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordEntry {
+
+    private String partOfSpeech;
+    private List<String> definitions;
+
+    public WordEntry(String partOfSpeech, List<String> definitions) {
+        this.partOfSpeech = partOfSpeech;
+        this.definitions = definitions;
+    }
+
+    public String getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public List<String> getDefinitions() {
+        return definitions;
+    }
+
+}
