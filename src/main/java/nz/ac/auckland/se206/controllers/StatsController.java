7e84d9d90ac68c19f352e724fc417b2e56c0b03c
Manjot <mman755@aucklanduni.ac.nz>
2022-10-12 16:17:11 +1300

    Added comments for StatsController.java class where needed

--- src/main/java/nz/ac/auckland/se206/controllers/StatsController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/StatsController.java
@@ -82,29 +82,37 @@ public class StatsController {
 
   @FXML
   private void onBadgeDisplaySwitch(ActionEvent event) throws IOException {
+    // Create a BadgesController class object
     BadgesController badgesController =
         (BadgesController) SceneManager.getUiController(SceneManager.AppUi.BADGES);
+    // Set the badges in the class for the current user by passing in the username
     badgesController.setBadgesForUser(userName);
-
+    // Create scene
     Scene scene = ((Node) event.getSource()).getScene();
+    // Set the root to load the badges to user
     scene.setRoot(SceneManager.getUiRoot(SceneManager.AppUi.BADGES));
   }
 
   private void updateWordsPlayedLabel(String difficulty, int numWords, User currentUser)
       throws IOException, URISyntaxException, CsvException {
+    // Create CategorySelector instance
     new CategorySelector();
+    // Create StringBuilder instance
     StringBuilder sb = new StringBuilder();
+    // Loop through each word in the current users word list and build the label string
     for (String word : currentUser.getWordList()) {
       if (difficulty.equals("N") || CategorySelector.getWordDifficulty(word).equals(difficulty)) {
         sb.append(word).append(System.getProperty("line.separator"));
       }
     }
+    // Switch statement to set lavel based on current difficulty of word
     switch (difficulty) {
       case "E" -> wordsPlayedLabel.setText(currentUser.getNumEasyWords() + "/" + numWords);
       case "M" -> wordsPlayedLabel.setText(currentUser.getNumMediumWords() + "/" + numWords);
       case "H" -> wordsPlayedLabel.setText(currentUser.getNumHardWords() + "/" + numWords);
       case "N" -> wordsPlayedLabel.setText(currentUser.getNumWordsPlayed() + "/" + numWords);
     }
+    // Set the wordList label to our constructed StringBuilder object
     wordList.setText(sb.toString());
   }
 }
